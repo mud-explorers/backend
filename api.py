234e449a7815a66fb6a9e72a09bb106b745d4df1
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-29 13:23:54 -0400

    added room class implementation

--- api.py
+++ api.py
@@ -12,8 +12,61 @@ with open('./.key') as f:
 
 
 class Room(object):
-    def __init__(self):
-        pass
+    def __init__(self, id=0, x=None, y=None, title="", description="", elevation=0, terrain=""):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.title = title
+        self.description = description
+        self.elevation = elevation
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.terrain = terrain
+
+    def get_exits(self):
+        exits = []
+        if self.n_to is not None:
+            exits.append('n')
+        if self.s_to is not None:
+            exits.append('s')
+        if self.e_to is not None:
+            exits.append('e')
+        if self.w_to is not None:
+            exits.append('w')
+        return exits
+
+    def connect_rooms(self, direction, connecting_room):
+        if direction == 'n':
+            self.n_to = connecting_room
+            connecting_room.s_to = self
+        elif direction == 's':
+            self.s_to = connecting_room
+            connecting_room.n_to = self
+        elif direction == 'e':
+            self.e_to = connecting_room
+            connecting_room.w_to = self
+        elif direction == 'w':
+            self.w_to = connecting_room
+            connecting_room.e_to = self
+        else:
+            return None
+
+    def get_room_in_direction(self, direction):
+        if direction == 'n':
+            return self.n_to
+        elif direction == 's':
+            return self.s_to
+        elif direction == 'e':
+            return self.e_to
+        elif direction == 'w':
+            return self.w_to
+        else:
+            return None
+
+    def coords(self):
+        return self.x, self.y
 
 
 class Graph(object):
