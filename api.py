5fa49d39272a289f6925b09254c510c4faa22fe3
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-29 13:07:41 -0400

    updated api - added init & move endpoints, added empty classes

--- api.py
+++ api.py
@@ -6,8 +6,23 @@ from urllib.parse import urlparse
 import requests
 import sys
 
+apikey = None
+with open('./.key') as f:
+    apikey = f.readlines()[0]
 
-class API(object):
+
+class Room(object):
+    def __init__(self):
+        pass
+
+
+class Graph(object):
+    def __init__(self):
+        self.rooms = dict()
+        self.visited_rooms = set()
+
+
+class Player(object):
     def __init__(self):
         pass
 
@@ -18,3 +33,22 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def root_route():
     return jsonify({'message': 'API ok'}), 200
+
+
+@app.route('/init', methods=['GET'])
+def init_route():
+    url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/'
+    headers = {"Authorization": f"Token {apikey}"}
+    r = requests.get(url=url, headers=headers)
+    return jsonify(r.json()), 200
+
+
+@app.route('/move', methods=['GET'])
+def move():
+    values = request.get_json()
+    diretion = values.get('direction')
+
+    url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/'
+    headers = {"Authorization": f"Token {apikey}"}
+    r = requests.get(url=url, headers=headers)
+    return jsonify(r.json()), 200
